061f81facc611810c86a4e2e3cd48e18c54898fb
NinhTheLight <67154739+NinhTheLight@users.noreply.github.com>
2021-10-20 15:16:27 +0700

    Ninh
    hihi

---
+++ QUẢN LÝ TOUR DU LỊCH/Form_ChiTietThongTin_Doan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUẢN_LÝ_TOUR_DU_LỊCH
+{
+    public partial class Form_ChiTietThongTin_Doan : Form
+    {
+        public Form_ChiTietThongTin_Doan()
+        {
+            InitializeComponent();
+        }
+    }
+}
